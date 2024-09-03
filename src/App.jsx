acaf45df4880dccbff4bf936d77cc3b6aa9cfce2
Hrishikesh Bade <hrishikeshbade007@gmail.com>
2024-09-03 12:11:09 +0530

    addition of a new font

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 // import React from 'react';
 import "./App.css";
+import {Routes, Route} from 'react-router-dom'
 import NavBar from "./components/NavBar";
 import PersonalInfoSignup from "./components/PersonalInfoSignup";
 import TrackExercise from "./components/TrackExercise";
+import About from './components/About';
+
 function App() {
   console.log("Hello World!");
   return (
     <>
-      <h1>Josh - The Fitness App</h1>
+      <h1 className="font-text">Josh - The Fitness App</h1>
       <NavBar></NavBar>
-      <PersonalInfoSignup></PersonalInfoSignup>
-      <TrackExercise></TrackExercise>
+      <Routes>
+        <Route exact path="/" element={<PersonalInfoSignup />} />
+        <Route exact path="/track-exercise" element={<TrackExercise />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+      
+      
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import {Link} from 'react-router-dom';
 function ColorSchemesExample() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">JOSH</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Personl Info</Nav.Link>
-            <Nav.Link href="#features">Track Exercise</Nav.Link>
-            <Nav.Link href="#pricing">About</Nav.Link>
+          <Nav className="me-auto ">
+            <Link className="navText" to="/">Personal Info</Link>
+            <Link className="navText"  to="/track-exercise">Track Exercise</Link>
+            <Link className="navText"  to="/about">About</Link>
           </Nav>
         </Container>
       </Navbar>
